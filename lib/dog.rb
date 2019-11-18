fae03b7e48bde34cabd0e6be4e3b716f1e5dda84
Sean Tarzy <seantarzy@gmail.com>
2019-11-18 23:05:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,3 +22,27 @@ def breed
 end 
 end 
 
+class Person 
+
+def Initialize(person_name, person_job)
+  @name = person_name
+  @job = person_job
+end 
+
+def name=(person_name)
+  @name = person_name
+end 
+
+def name 
+  @name 
+end 
+
+def job=(person_job)
+  @job = person_job
+end 
+
+def job
+  @job
+end 
+end 
+
