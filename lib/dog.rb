d17b559180d2ac1781c643cae44754ef4beaecf7
Sean Tarzy <seantarzy@gmail.com>
2019-11-18 22:58:24 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+ class Dog 
+
+def Initialize(dog_name)
+  @name = dog_name
+end 
+
+def name=(dog_name)
+  @name = dog_name 
+end 
+
+def name 
+  @name 
+def Initialize(dog_breed)
+  @breed = breed
+end 
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end 
+
+def breed
+  @breed
+end 
+end 
+
