c54c9467109e9ca6937579a5be317c6034c8bdac
Sean Tarzy <seantarzy@gmail.com>
2019-11-18 23:05:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,48 @@
+class Dog 
+
+def Initialize(dog_name, dog_breed)
+  @name = dog_name
+  @breed = dog_breed
+end 
+
+def name=(dog_name)
+  @name = dog_name 
+end 
+
+def name 
+  @name 
+end 
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end 
+
+def breed
+  @breed
+end 
+end 
+
+class Person 
+
+def Initialize(person_name, person_job)
+  @name = person_name
+  @job = person_job
+end 
+
+def name=(person_name)
+  @name = person_name
+end 
+
+def name 
+  @name 
+end 
+
+def job=(person_job)
+  @job = person_job
+end 
+
+def job
+  @job
+end 
+end 
+
